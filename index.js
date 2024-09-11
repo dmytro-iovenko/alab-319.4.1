80c1e2a8b01de96870c35b9e5066bf0b7fd5f625
Dmytro Iovenko <iovenko@gmail.com>
2024-09-11 15:36:45 -0400

    add start code

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 import express from "express";
-const app = express();
-const port = 3000;
 
-// Start express server
-app.listen(port, () => console.log("Server is running on port:", port));
+const PORT = 5050;
+const app = express();
+
+import grades from "./routes/grades.js";
+import grades_agg from "./routes/grades_agg.js";
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the API.");
+});
+
+app.use("/grades", grades);
+app.use("/grades", grades_agg);
+
+// Global error handling
+app.use((err, _req, res, next) => {
+  res.status(500).send("Seems like we messed up somewhere...");
+});
+
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
